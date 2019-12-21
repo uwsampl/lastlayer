3fae9e6b97fe5fbe482b88c194b6fd873095c645
Luis Vega <vegaluisjose@gmail.com>
2019-12-20 22:51:30 -0800

    normalize cycles

--- numpy/adder.py
+++ numpy/adder.py
@@ -2,7 +2,7 @@ from device import Device
 
 dev = Device("build/libadder.so")
 dev.reset(20)
-# dev.write(0, 0, 3)
-dev.run(100)
-dev.run(100)
+dev.write(0, 0, 3)
+dev.run(4)
+dev.run(5)
 print(dev.read(0, 0))
