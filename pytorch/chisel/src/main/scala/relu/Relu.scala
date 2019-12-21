14f5035c86e6dc50efb0c25f2ee9be0dad5102a6
Luis Vega <vegaluisjose@gmail.com>
2019-12-21 09:39:44 -0800

    update

--- pytorch/chisel/src/main/scala/relu/Relu.scala
+++ pytorch/chisel/src/main/scala/relu/Relu.scala
@@ -2,12 +2,33 @@ package relu
 
 import chisel3._
 
-class Relu extends Module {
+case class ReluConfig() {
+  val dataWidth = 8
+}
+
+class Op(implicit config: ReluConfig) extends Module {
   val io = IO(new Bundle {
-    val value = Input(UInt(16.W))
+    val a = Input(UInt(config.dataWidth.W))
+    val y = Output(UInt(config.dataWidth.W))
   })
+  io.y := Mux(io.a.asSInt > 0.S, io.a, 0.U)
+}
+
+class Relu(implicit config: ReluConfig) extends Module {
+  val io = IO(new Bundle {
+    val a = Input(UInt(config.dataWidth.W))
+    val y = Output(UInt(config.dataWidth.W))
+  })
+  val a = RegInit(0.U(config.dataWidth.W))
+  val y = RegInit(0.U(config.dataWidth.W))
+  val op = Module(new Op)
+  a := io.a
+  io.y := y
+  op.io.a := a
+  y := op.io.y
 }
 
 object Relu extends App {
+  implicit val config = ReluConfig()
   chisel3.Driver.execute(args, () => new Relu)
 }
