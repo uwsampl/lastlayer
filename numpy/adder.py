0a5cb1feb53f3b31ca12dadacd7ac7defb0cc44a
Luis Vega <vegaluisjose@gmail.com>
2019-12-20 21:17:54 -0800

    fix reset bug, still need to figure out the blk nblk for writes

--- numpy/adder.py
+++ numpy/adder.py
@@ -1,7 +1,8 @@
 from device import Device
 
 dev = Device("build/libadder.so")
-dev.reset(10)
-dev.write(0, 0, 3)
-dev.run(10)
+dev.reset(20)
+# dev.write(0, 0, 3)
+dev.run(100)
+# dev.run(100)
 print(dev.read(0, 0))
--- numpy/adder.v
+++ numpy/adder.v
@@ -2,4 +2,13 @@ module adder(input clock, input reset);
 
     reg [31:0] r0;
 
+    always @(posedge clock) begin
+        if (reset) begin
+            r0 <= 0;
+        end
+        else begin
+            r0 <= r0 + 1;
+        end
+    end
+
 endmodule
