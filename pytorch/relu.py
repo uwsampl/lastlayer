6c1d8aed71dbaf544940e3bc183c4cf38e8d41aa
Luis Vega <vegaluisjose@gmail.com>
2019-12-21 20:47:43 -0800

    perform one relu op

--- pytorch/relu.py
+++ pytorch/relu.py
@@ -6,14 +6,15 @@ n = 8
 minv = -64
 maxv = 64
 
-dev = Device("build/librelu.so")
 nx = np.random.randint(minv, maxv, size=n, dtype="int8")
 tx = torch.tensor(nx, dtype=torch.int8)
-print(tx)
+
+dev = Device("build/librelu.so")
+dev.reset(3)
 dev.write_mem(0, 0, tx)
-ty = dev.read_mem(0, 0, tx.numel())
-print(ty)
-# dev.reset(10)
-# dev.write_reg(0, 0, -3)
-# dev.run(20)
-# print("result:{}".format(dev.read_reg(1, 0)))
+dev.write_reg(0, 0, 1)
+dev.write_reg(1, 0, 3)
+dev.run(5)
+ty = dev.read_mem(1, 0, tx.numel())
+print("input:", tx)
+print("output:", ty)
