2bb2f490b77e7056a80cadca41ef829db59be93a
Luis Vega <vegaluisjose@gmail.com>
2019-12-21 17:16:34 -0800

    update

--- pytorch/chisel/src/main/scala/relu/Relu.scala
+++ pytorch/chisel/src/main/scala/relu/Relu.scala
@@ -1,28 +1,46 @@
 package relu
 
 import chisel3._
+import chisel3.util._
 
 case class ReluConfig() {
-  val dataWidth = 8
+  val xLen = 1
+  val opDataWidth = 8
+  val memDepth = 1024
+  val memDataWidth = xLen * opDataWidth
+  val memAddrWidth = log2Ceil(memDepth)
 }
 
 class Op(implicit config: ReluConfig) extends Module {
   val io = IO(new Bundle {
-    val a = Input(UInt(config.dataWidth.W))
-    val y = Output(UInt(config.dataWidth.W))
+    val a = Input(UInt(config.opDataWidth.W))
+    val y = Output(UInt(config.opDataWidth.W))
   })
   io.y := Mux(io.a.asSInt > 0.S, io.a, 0.U)
 }
 
 class Relu(implicit config: ReluConfig) extends Module {
-  val io = IO(new Bundle{})
-  val a = RegInit(0.U(config.dataWidth.W))
-  val y = RegInit(0.U(config.dataWidth.W))
+  val io = IO(new Bundle{
+    val wen = Input(Bool())
+    val waddr = Input(UInt(config.memAddrWidth.W))
+    val wdata = Input(UInt(config.memDataWidth.W))
+    val ren = Input(Bool())
+    val raddr = Input(UInt(config.memAddrWidth.W))
+    val rdata = Output(UInt(config.memDataWidth.W))
+  })
+  val raddr = RegInit(0.U(config.memAddrWidth.W))
+  val waddr = RegInit(0.U(config.memAddrWidth.W))
   val op = Module(new Op)
-  op.io.a := a
-  y := op.io.y
-  dontTouch(a)
-  dontTouch(y)
+  val rmem = SyncReadMem(config.memDepth, UInt(config.memDataWidth.W))
+  val wmem = SyncReadMem(config.memDepth, UInt(config.memDataWidth.W))
+  op.io.a := rmem.read(raddr, true.B)
+  wmem.write(waddr, op.io.y)
+  when (io.wen) {
+    rmem.write(io.waddr, io.wdata)
+  }
+  io.rdata := wmem.read(io.raddr, io.ren)
+  dontTouch(raddr)
+  dontTouch(waddr)
 }
 
 object Relu extends App {
